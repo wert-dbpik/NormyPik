9808e856e80f82791fa45c761111178845d1e216
Aleksei V. Simonov <wert001@yandex.ru>
2022-09-10 21:04:22 +0300

    create main activity

---
+++ app/src/main/java/ru/wert/normypik/MainActivity.kt
@@ -0,0 +1,25 @@
+package ru.wert.normypik
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val logo: ImageView = findViewById(R.id.imageViewLogo);
+
+        logo.setOnClickListener {
+            val startApplication = Intent(this, StartActivity::class.java);
+            startActivity(startApplication);
+
+        }
+
+    }
+}
